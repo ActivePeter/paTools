9bbcb09e70b28e5888d515286a0a03580ef27191
ActivePeter <1020401660@qq.com>
2023-02-15 14:49:51 +0800

    try change ssh in cron

--- datenlord_local/1_setup_ssh.sh
+++ datenlord_local/1_setup_ssh.sh
@@ -1,16 +1,17 @@
 NODES_IP="$(kubectl get nodes -A -o wide | awk 'FNR > 2 {print $6}')"
 NODES="$(kubectl get nodes -A -o wide | awk 'FNR > 2 {print $1}')"
 for node in ${NODES}; do
-  USER="$(whoami)"
+  USER="pa"
   docker cp /etc/apt/sources.list ${node}:/etc/apt/sources.list
   docker exec ${node} apt-get update
   docker exec ${node} apt-get install -y ssh sudo
+  docker exec ${node} systemctl enable sshd
   docker exec ${node} systemctl start sshd
   docker exec ${node} useradd -m ${USER}
   docker exec ${node} usermod -aG sudo ${USER}
   echo "${USER} ALL=(ALL) NOPASSWD:ALL" > /tmp/${USER}
-  docker cp /tmp/${USER} ${node}:/etc/sudoers.d/${USER}
-  docker exec ${node} chown root:root /etc/sudoers.d/${USER}
+  # docker cp /tmp/${USER} ${node}:/etc/sudoers.d/${USER}
+  # docker exec ${node} chown root:root /etc/sudoers.d/${USER}
 
   docker exec ${node} mkdir /home/${USER}/.ssh
   docker exec ${node} ls -al /home/${USER}/
