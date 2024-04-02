fc9c0ac7ec5c77875a40e8c9efaea6e164baf98c
ActivePeter <1020401660@qq.com>
2024-04-02 08:40:50 +0000

    refactor: proxy config by yaml

---
+++ config_proxy/shell_export.py
@@ -0,0 +1,31 @@
+import os
+import yaml
+
+CUR_FPATH = os.path.abspath(__file__)
+CUR_FDIR = os.path.dirname(CUR_FPATH)
+# chdir to the directory of this script
+os.chdir(CUR_FDIR)
+
+
+# 打开并读取YAML文件
+with open('proxy.yaml', 'r') as file:
+    # 加载YAML数据
+    data = yaml.safe_load(file)
+HOST=data['host']
+PORT=data['port']
+
+
+# # docker compose up
+# os.system('git config --global http.proxy http://'+HOST+':'+PORT)
+# os.system('git config --global https.proxy http://'+HOST+':'+PORT)
+# # os.system('ex')
+
+
+
+
+print(
+f'''
+export http_proxy=http://{HOST}:{PORT}
+export https_proxy=http://{HOST}:{PORT}
+'''
+)
