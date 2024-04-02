b54d6c951c706924d392a6d19f0652e33b4ed277
ActivePeter <1020401660@qq.com>
2024-04-02 08:44:25 +0000

    refactor: remove no use codes

--- datenlord_local/redeploy_with_build.sh
+++
@@ -1,6 +0,0 @@
-# run in prj root dir
-kind delete cluster
-kind create cluster --config ./paTools/k8s_kind/kind-config.yaml
-docker build . --build-arg RUST_IMAGE_VERSION=1.61.0 --file ./Dockerfile --target datenlord --tag datenlord/datenlord:e2e_test
-kind load docker-image datenlord/datenlord:e2e_test
-sh ./temp.sh
